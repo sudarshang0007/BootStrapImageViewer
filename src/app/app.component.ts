f3c3f84ccb3f9a2858205b3a9b470be68a679bca
Sudarshan <sudarshang0007@gmail.com>
2018-07-03 21:59:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  images = [];
+  selectedImage: any;
+  selecteImageIndex: number;
+
+ constructor(){
+   this.images.push({src:'https://www.artsfon.com/large/201412/38406.jpg?img='+ Math.random()});
+   this.images.push({src:'https://wallpapersite.com/images/wallpapers/forest-3000x1875-path-hd-4221.jpg?img='+ Math.random()});
+   this.images.push({src:'https://www.artsfon.com/large/201412/38406.jpg?img='+ Math.random()});
+   this.images.push({src:'https://wallpapersite.com/images/wallpapers/forest-3000x1875-path-hd-4221.jpg?img='+ Math.random()});
+   this.images.push({src:'https://www.artsfon.com/large/201412/38406.jpg?img='+ Math.random()});
+   this.images.push({src:'https://wallpapersite.com/images/wallpapers/forest-3000x1875-path-hd-4221.jpg?img='+ Math.random()});
+   this.images.push({src:'https://www.artsfon.com/large/201412/38406.jpg?img='+ Math.random()});
+   this.images.push({src:'https://wallpapersite.com/images/wallpapers/forest-3000x1875-path-hd-4221.jpg?img='+ Math.random()});
+   this.images.push({src:'https://www.artsfon.com/large/201412/38406.jpg?img='+ Math.random()});
+   this.images.push({src:'https://wallpapersite.com/images/wallpapers/forest-3000x1875-path-hd-4221.jpg?img='+ Math.random()});
+   this.images.push({src:'https://www.artsfon.com/large/201412/38406.jpg?img='+ Math.random()});
+   this.images.push({src:'https://wallpapersite.com/images/wallpapers/forest-3000x1875-path-hd-4221.jpg?img='+ Math.random()});
+   this.images.push({src:'https://www.artsfon.com/large/201412/38406.jpg?img='+ Math.random()});
+   this.images.push({src:'https://wallpapersite.com/images/wallpapers/forest-3000x1875-path-hd-4221.jpg?img='+ Math.random()});
+   
+ }
+
+
+
+  displayImage(i):  void{
+    console.log(i, this.images[i]);
+    this.selecteImageIndex = i;
+    this.selectedImage = this.images[i];
+  }
+
+  prevImage() : void{
+    this.selecteImageIndex = this.selecteImageIndex == 0 ? this.images.length - 1 : this.selecteImageIndex - 1; 
+    this.selectedImage = this.images[this.selecteImageIndex];
+  }
+
+  nextImage(): void{
+    this.selecteImageIndex = this.selecteImageIndex == this.images.length - 1 ? 0 : this.selecteImageIndex + 1; 
+    this.selectedImage = this.images[this.selecteImageIndex];
+  }
+
+  imageLoad() : void{
+    console.log('imageLoading');
+  }
+
+  removeSpinner(divId: string): void{
+      document.getElementById(divId).setAttribute('style','display:none;');
+  }
+
 }
